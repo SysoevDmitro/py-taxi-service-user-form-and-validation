68cc89330a9416a6a8becb1159fb728f47713456
SysoevDmitro <113758312+SysoevDmitro@users.noreply.github.com>
2024-04-16 17:27:43 +0300

    Update forms.py

--- taxi/forms.py
+++ taxi/forms.py
@@ -14,14 +14,11 @@ class DriverLicenseCreateForm(UserCreationForm):
     def clean_license_number(self):
         license_number = self.cleaned_data["license_number"]
         if len(license_number) != 8:
-            raise forms.ValidationError(
-                "License number must consist of 8 characters.")
-        if not license_number[:3].isupper():
-            raise forms.ValidationError(
-                "First 3 characters must be uppercase letters.")
-        if not license_number[3:].isdigit():
-            raise forms.ValidationError(
-                "Last 5 characters must be digits.")
+            raise ValidationError("License number should consist of 8 characters")
+        if not license_number[:3].isalpha() or not license_number[:3].isupper():
+            raise ValidationError("First 3 characters should be uppercase letters")
+        if license_number[:3].isupper() and not license_number[-5:].isdigit():
+            raise ValidationError("Last 5 characters should be digits")
         return license_number
 
 
@@ -34,13 +31,11 @@ class DriverLicenseUpdateForm(forms.ModelForm):
     def clean_license_number(self):
         license_number = self.cleaned_data["license_number"]
         if len(license_number) != 8:
-            raise forms.ValidationError(
-                "License number must consist of 8 characters.")
-        if not license_number[:3].isupper() or license_number[:3].isdigit():
-            raise forms.ValidationError(
-                "First 3 characters must be uppercase letters.")
-        if not license_number[3:].isdigit():
-            raise forms.ValidationError("Last 5 characters must be digits.")
+            raise ValidationError("License number should consist of 8 characters")
+        if not license_number[:3].isalpha() or not license_number[:3].isupper():
+            raise ValidationError("First 3 characters should be uppercase letters")
+        if license_number[:3].isupper() and not license_number[-5:].isdigit():
+            raise ValidationError("Last 5 characters should be digits")
         return license_number
 
 
